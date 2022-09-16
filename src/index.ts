7c054da8e2c934a31ceda53102d6314afe1479de
chonan <hoge@hoge.com>
2022-09-17 08:54:49 +0900

    ゆるきゃらをリスト取得完成した

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,41 @@
 import {Response} from "request";
-
 const request = require('request')
-const { JSDOM } = require('jsdom')
+const {JSDOM} = require('jsdom')
 
-const url = 'https://sugotoku-yurugp.secureserv.jp/character/nameList.php';
+export type Character = {
+    name: string;
+    url: string;
+}
 
-request(url, (e: any, response: Response, body: any) => {
-    if (e) {
-        console.error(e)
-    }
+export const getCharacterList = () => {
+    const url = 'https://sugotoku-yurugp.secureserv.jp/character/nameList.php';
 
-    try {
-        const dom = new JSDOM(body)
+    request(url, (e: any, response: Response, body: any) => {
+        if (e) {
+            console.error(e)
+        }
+        const characterList: Character[] = [];
 
-        const characterNameDivs = [...(dom.window.document.querySelectorAll('.name'))];
-        characterNameDivs.map((characterNameDiv:any) => {
-            console.log(characterNameDiv.textContent)
-        });
-    } catch (e) {
-        console.error(e)
-    }
-})
+        try {
+            const dom = new JSDOM(body)
+
+            // キャラクタの画像URLリスト
+            const characterThumbDivs = [...(dom.window.document.querySelectorAll('.thumbnail'))];
+            characterThumbDivs.map((characterThumbDiv: any) => {
+                const imgSrcUrl = characterThumbDiv.children[0].src as string;
+                const imgSrcName = characterThumbDiv.children[0].alt as string;
+                characterList.push({
+                    name: imgSrcName,
+                    url: imgSrcUrl
+                });
+            });
+            console.log(characterList);
+        } catch (e) {
+            console.error(e)
+        }
+    })
+};
+
+getCharacterList();
+
+
